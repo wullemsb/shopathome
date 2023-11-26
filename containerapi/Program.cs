56eb5fb21ab16ae2ea96036d33903da017aaff80
Wullems Bart <wullemsb@gmail.com>
2023-11-26 16:09:31 +0100

    Return name from api

--- containerapi/Program.cs
+++ containerapi/Program.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,21 +18,21 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGet("/api/hello", ([FromHeader(Name= "X-MS-CLIENT-PRINCIPAL")] string? token) =>
 {
-    return $"hello world - {token}";
+    //decode base64 token
+    if(token is not null)
+    {
+        var decodedToken = Convert.FromBase64String(token);
+        var decodedString = Encoding.UTF8.GetString(decodedToken);
+        var tokenObject = JsonSerializer.Deserialize<Dictionary<string, string>>(decodedString);
+        var name = tokenObject["userDetails"];
+        return $"hello {name}";
+    }
+
+    return $"hello world-{token}";
 })
 .WithName("GetHello")
 .WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
